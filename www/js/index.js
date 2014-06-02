8a7113b61af7ceaab05b9705e1a248945c96c30b
filipemoreira23 <filipemoreira23@gmail.com>
2014-06-02 15:46:36 -0300

    First

---
+++ www/js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	var count = 0;
+	$("#btn-req").click(function(){
+		/*$.get("https://estruturaorganizacional.dados.gov.br/siorg-servicos-webapp/doc/unidade-organizacional/002981",function(data){
+				console.log(data);
+				console.log("OI");
+		},"xml");*/
+	 	$.ajax({
+	        url: "http://192.168.0.114/resttest/test.php",
+	        type: "GET",
+	        dataType: "json",
+	        success: function(response){
+	            //appendTable(response);
+	            console.log(response);
+				count++;
+				$("#response-table").append("<tr><td>"+count+"</td><td>"+response.servico.codigoErro +"</td><td>"
+					+response.servico.ipRequisitante+"</td><td>"+response.servico.mensagem+"</td></tr>");
+				$("#response-table").trigger("refresh");
+	        }
+	    });
+	});
+});
